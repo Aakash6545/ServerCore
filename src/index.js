2bde36bad9a4836e9b5cff76f642cfaef92e39df
Aakash <aakashsheoran592@gmail.com>
2024-12-28 21:39:44 +0530

    set up Express app and integrate with database connection

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
-import express from "express";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import {app} from "./app.js";
 
-// Load environment variables
 dotenv.config({
   path: "./.env",
 });
 
-const app = express();
 
-// Connect to the database
+
 connectDB()
   .then(() => {
-    // Only start the server if the database connection is successful
     app.listen(process.env.PORT, () => {
       console.log(`App is listening on port ${process.env.PORT}`);
     });
   })
   .catch((error) => {
-    // Log the error and stop the app from starting if the database connection fails
     console.error("Error connecting to the database:", error.message);
-    process.exit(1);  // Exit the application with a failure code
+    process.exit(1);  
   });
 
-  
